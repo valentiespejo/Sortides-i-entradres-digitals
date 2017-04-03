349525ce5549979c083fce312f600e48417d3933
valentiespejo <valentiesro@gmail.com>
2017-04-03 21:28:57 +0200

    Cub

---
+++ Cub de 3x3x3 LEDs/Comprovacio_Led/Comprovacio_Led.ino
@@ -0,0 +1,65 @@
+int col1 = 2;
+int col2 = 3;
+int col3 = 4;
+int col4 = 5;
+int col5 = 6;
+int col6 = 7;
+int col7 = 8;
+int col8 = 9;
+int col9 = 10;
+int niv1 = 11;
+int niv2 = 12;
+int niv3 = 13;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(col1, OUTPUT);
+  pinMode(col2, OUTPUT);
+  pinMode(col3, OUTPUT);
+  pinMode(col4, OUTPUT);
+  pinMode(col5, OUTPUT);
+  pinMode(col6, OUTPUT);
+  pinMode(col7, OUTPUT);
+  pinMode(col8, OUTPUT);
+  pinMode(col9, OUTPUT);
+  pinMode(niv1, OUTPUT);
+  pinMode(niv2, OUTPUT);
+  pinMode(niv3, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  digitalWrite(col1, HIGH);
+  digitalWrite(niv1, HIGH);
+  delay(500);
+  digitalWrite(col1, LOW);
+  digitalWrite(col2, HIGH);
+  delay(500);
+  digitalWrite(col2, LOW);
+  digitalWrite(col3, HIGH);
+  delay(500);
+  digitalWrite(col3, LOW);
+  digitalWrite(col4, HIGH);
+  delay(500);
+  digitalWrite(col4, LOW);
+  digitalWrite(col5, HIGH);
+  delay(500);
+  digitalWrite(col5, LOW);
+  digitalWrite(col6, HIGH);
+  delay(500);
+  digitalWrite(col6, LOW);
+  digitalWrite(col7, HIGH);
+  delay(500);
+  digitalWrite(col7, LOW);
+  digitalWrite(col8, HIGH);
+  delay(500);
+  digitalWrite(col8, LOW);
+  digitalWrite(col9, HIGH);
+  delay(500);
+  digitalWrite(col9,LOW);
+  digitalWrite(niv1, LOW);
+  delay(500);
+
+}
--- Cub de 3x3x3 LEDs/Cub_de_3x3x3_LEDs/Cub_de_3x3x3_LEDs.ino
+++ Cub de 3x3x3 LEDs/Cub_de_3x3x3_LEDs/Cub_de_3x3x3_LEDs.ino
@@ -8,11 +8,7 @@
 //******************************* INCLUDES ****************************************
 
 //******************************* VARIABLES ***************************************
-/*
-    ledcube.pde - Example sketch for controlling an LED cube.
-    Created by Gamaiel Zavala (gzip), 2009-2014
-    MIT License. See accompanying LICENSE file for terms.
-*/
+
 
 #include <LedCube.h>
 
@@ -55,7 +51,7 @@ void loop ()
 #ifdef DEBUG
     showmem("start");
 #endif
-    // paste UI output here (https://rawgithub.com/gzip/arduino-ledcube/master/ui/index.html)
+
     cubeFrame* f[] = {
         cube.createFrame((const byte[]) {0,6, 1,6, 2,6}, 6, 80),
         cube.createFrame((const byte[]) {0,7, 1,7, 2,7}, 6, 70),
